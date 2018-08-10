766e7744b4f93c8a25729b5a35c60629b4a25f40
VaniushaR <vaniusha.ra@gmail.com>
2018-08-10 15:10:08 -0500

    Agregando administrador login y maquetado

--- src/js/adm-login.js
+++ src/js/adm-login.js
@@ -1,16 +1,24 @@
-//Login cerrado para perfiles muy específicos
-loginView.style.display = 'none';
 const admin = document.getElementById('nombre-admin');
 const password = document.getElementById('password');
-const loginView = document.getElementById('login-section');
-
+const adminView = document.getElementById('login-section');
+const logIn = document.getElementById('log-in');
 const administrador = document.getElementById('admin');
-administrador.addEventListener('click', login);
+const logButton = document.getElementById('btn-log');
+const welcomeAdmin = documen.getElementById('welcome-admin');
+
+loginView.style.display = 'none';
+logButton.addEventListener('click', login);
+
+//Login cerrado para perfiles muy específicos
 
 const login = () => {
-If (admin.value == 'administrador' && password.value == '12345') {
-loginView.style.display = 'block';
-
-} else {
-    alert('Usuario o contraseña incorrecta. Sólo un administrador autorizado puede tener acceso.');
-}
+  if (admin.value === 'administrador' && password.value === '12345') {
+    adminView.style.display = 'block';
+    logIn.style.display = 'none';
+    welcomeAdmin.innerHTML = admin + '!';
+  } else {
+    alert(
+      'Usuario o contraseña incorrecta. Sólo un administrador autorizado puede tener acceso.'
+    );
+  }
+};
