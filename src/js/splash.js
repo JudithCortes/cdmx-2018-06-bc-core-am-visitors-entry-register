6918adb6cfeca976fb7808bdb0cb9169004c8edd
VaniushaR <vaniusha.ra@gmail.com>
2018-08-13 11:04:35 -0500

    Agregando detalles de interfaz y login

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,5 +1,6 @@
 //Función que determina el tiempo de duración de la pantalla Splash
 splashView = () => {
-  setTimeout(() => (location.href = '../index.html'), 2000);
+  setTimeout(() => (location.href = '../index.html'), 3000);
 };
-splashView();
+
+// splashView();
